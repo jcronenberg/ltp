b7d4dc69d18056bb68b26887dc8150bf367e9202
Jorik Cronenberg <jcronenberg@suse.de>
2020-01-15 17:01:28 +0100

    fix license identifier

--- testcases/kernel/syscalls/timerfd/timerfd01.c
+++ testcases/kernel/syscalls/timerfd/timerfd01.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  *  timerfd() test by Davide Libenzi (test app for timerfd)
  *  Copyright (C) 2007  Davide Libenzi
