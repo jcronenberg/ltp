4777bfbe31e39752f33ae56cc6a8fb44095acedc
Jorik Cronenberg <jcronenberg@suse.de>
2020-01-16 16:37:55 +0100

    I guess this works, but sleep over it for a night

--- testcases/kernel/syscalls/timer_gettime/timer_gettime01.c
+++ testcases/kernel/syscalls/timer_gettime/timer_gettime01.c
@@ -15,58 +15,46 @@
 #include "tst_test.h"
 #include "lapi/syscalls.h"
 
-static struct tcase {
-	int exp_err;
-	int timer;
-	struct itimerspec spec;
-	char const *name;
-} tcases[] = {
-	{
-	 .exp_err = 0,
-	 .timer = 0,
-	 .name = "timer_gettime(CLOCK_REALTIME)",
-	 },
-	{
-	 .exp_err = EINVAL,
-	 .timer = -1,
-	 .name = "timer_gettime(minusone)",
-	 },
-	{
-	 .exp_err = EFAULT,	 
-	 .timer = 0,
-	 .spec = ,
-	 .name = "timer_gettime(NULL)",
-	 },
-};
-
-static void run(unsigned int n)
+static void run(void)
 {
 	struct sigevent ev;
-	struct tcase *tc = &tcases[n];
+	struct itimerspec spec;
+	int timer;
 	
 	ev.sigev_value = (union sigval) 0;
 	ev.sigev_signo = SIGALRM;
 	ev.sigev_notify = SIGEV_SIGNAL;
-	if(!n) {
-		TEST(tst_syscall(__NR_timer_create, CLOCK_REALTIME, &ev, &tc->timer));
-		if (TST_RET != 0)
-			tst_brk(TBROK | TERRNO, "Failed to create timer");
-	} 
 
-	TEST(tst_syscall(__NR_timer_gettime, tc->timer, &tc->spec));
+	TEST(tst_syscall(__NR_timer_create, CLOCK_REALTIME, &ev, &timer));
+	if (TST_RET != 0)
+		tst_brk(TBROK | TERRNO, "Failed to create timer");
+
+	TEST(tst_syscall(__NR_timer_gettime, timer, &spec));
+	if (TST_RET == 0) {
+		tst_res(TPASS, "timer_gettime(CLOCK_REALTIME) Passed");
+	} else {
+		tst_res(TFAIL | TERRNO,
+		         "timer_gettime(CLOCK_REALTIME) Failed");
+	}
+
+	TEST(tst_syscall(__NR_timer_gettime, -1, &spec));
+	if (TST_RET == -1 && TST_ERR == EINVAL) {
+		tst_res(TPASS,	"timer_gettime(-1) Failed: EINVAL");
+	} else {
+		tst_res(TFAIL | TERRNO,
+		         "timer_gettime(-1) = %li", TST_RET);
+	}
+
+	TEST(tst_syscall(__NR_timer_gettime, timer, NULL));
+	if (TST_RET == -1 && TST_ERR == EFAULT) {
+		tst_res(TPASS,	"timer_gettime(NULL) Failed: EFAULT");
+	} else {
+		tst_res(TFAIL | TERRNO,
+		         "timer_gettime(-1) = %li", TST_RET);
+	}
 	
-		if (TST_RET == tc->exp_err)
-			tst_res(TPASS, "%s Passed", tc->name);
-		else if (TST_RET == -1 && TST_ERR == tc->exp_err)
-			tst_res(TPASS,  "%s failed expectedly", tc->name);
-		else if (TST_RET == 0)
-			tst_res(TFAIL, "%s succeded unexpectedly", tc->name);
-		else
-			tst_res(TFAIL | TERRNO, "%s failed", tc->name);
-
 }
 
 static struct tst_test test = {
-	.test = run,
-	.tcnt = ARRAY_SIZE(tcases),
+	.test_all = run,
 };
